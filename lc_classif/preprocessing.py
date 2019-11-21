604405d5c2fbc7b2a30d438a3e5ac32de51fe50d
AntjeUhde <antjeuhde@web.de>
2019-11-21 21:46:25 +0100

    alterations on gdalwarp parameters for reprojection of mask

--- lc_classif/preprocessing.py
+++ lc_classif/preprocessing.py
@@ -2,16 +2,18 @@ import functions as f
 import rasterio
 from rasterio.enums import Resampling
 
-pol='VH'
+pols=['VH','VV']
 
-fp_stack=r"D:\Master\Geo419\Projekt\Daten\Sweden_Vattenrike_{}_2018".format(pol)
-fp_hdr=r"D:\Master\Geo419\Projekt\Daten\Sweden_Vattenrike_{}_2018.hdr".format(pol)
-fp_mask=r"D:\Master\Geo419\Projekt\Daten\clc2018_clc2018_v2018_20_raster100m\CLC2018_CLC2018_V2018_20.tif"
-outfnstack=r'D:\Master\Geo419\Projekt\Daten\Sweden_Vattenrike_{}_2018_100m'.format(pol)
-outfnmask=r'D:\Master\Geo419\Projekt\Daten\clc2018_vattenrike.tif'
+for pol in pols:
+    fp_stack=r"D:\Master\Geo419\Projekt\Daten\Sweden_Vattenrike_{}_2018".format(pol)
+    fp_hdr=r"D:\Master\Geo419\Projekt\Daten\Sweden_Vattenrike_{}_2018.hdr".format(pol)
+    fp_mask=r"D:\Master\Geo419\Projekt\Daten\clc2018_clc2018_v2018_20_raster100m\CLC2018_CLC2018_V2018_20.tif"
+    outfnstack=r'D:\Master\Geo419\Projekt\Daten\Sweden_Vattenrike_{}_2018_100m_test'.format(pol)
+    outfnmask=r'D:\Master\Geo419\Projekt\Daten\clc2018_vattenrike_test.tif'
 
-# adjust pixel size and extend of S-1 data and mask
-stack,mask=f.adjust(fp_stack,fp_mask, epsg=32633, write=True, outfp1=outfnstack,outfp2=outfnmask,hdrfp=fp_hdr,subset=False) #epsg=32633
+    # adjust pixel size and extend of S-1 data and mask
+    # stack,mask=f.adjust(fp_stack,fp_mask, epsg=32633, write=True, outfp1=outfnstack,outfp2=outfnmask,hdrfp=fp_hdr,subset=False) #epsg=32633
+    f.adjust(fp_stack,fp_mask, epsg=32633, write=True, outfp1=outfnstack,outfp2=outfnmask,hdrfp=fp_hdr,subset=False) #epsg=32633
 
 stack=None
 mask=None
