b13ae75bba64b4201578b2f44f9e1d4a304cfd94
AntjeUhde <antjeuhde@web.de>
2020-01-31 08:53:15 +0100

    adapted visualization to use rgb values and level 3 class names

--- lc_classif/visualization.py
+++ lc_classif/visualization.py
@@ -2,22 +2,51 @@
 """
 visualization.py: visualizing the results of Random Forest image classification
 
-@author: Theresa Möller
+@author: Theresa Möller, Antje Uhde
 """
 
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import matplotlib.patches as mpatches
+from matplotlib.patches import Patch
+import matplotlib as mpl
 import numpy as np
+import pandas as pd
 
-def plotResult(prediction, imagedim = [127,455]):
+def get_meta(number):
+    """
+    Retrieve the level 3 classname and official RGB value for each clc class.
+
+    Parameters
+    ----------
+    number: int
+        CLC_CODE of the class
+    
+    Examples
+    --------
+    >>> get_meta(111) 
+
+    Returns
+    -------
+    Official RGB colors as (r,g,b)
+    Level 3 name of the class
+    """
+    df=pd.read_csv(r'./data/clc_legend.txt', header=None)
+    rgb=((df.loc[df[0]==number,1].values[0]/255),(df.loc[df[0]==number,2].values[0]/255),(df.loc[df[0]==number,3].values[0]/255))
+    name=df.loc[df[0]==number,5].values[0]
+    return rgb,name
+
+def plotResult(prediction, imagedim = [127,455], fp=None):
     """
     Plot predicted image
 
     Parameters
     ----------
-    prediction: 1d-array with predicted labels        
-    imagedim: list containing height and width of resulting image
+    prediction: array
+        array with predicted labels        
+    imagedim: list
+        containing height and width of resulting image
+    fp: str (optional)
+        filepath to save the plot on disk
 
     Examples
     --------
@@ -29,13 +58,19 @@ def plotResult(prediction, imagedim = [127,455]):
     """
     grid = prediction.reshape((imagedim[0], imagedim[1]))
     values = np.unique(prediction.ravel())
-    plt.figure(figsize=(8,4))
-    im = plt.imshow(grid, interpolation='none')
-    #TODO: sort list of unique values and get the corresponding CMYK colors
-    # create custom colormap using matplotlib.colors.ListedColormap(['blue','black','red'])
-    # pass on this colormap to the plotting function
-    colors = [ im.cmap(im.norm(value)) for value in values]
-    patches = [ mpatches.Patch(color=colors[i], 
-                label="{l}".format(l=values[i]) ) for i in range(len(values)) ]
-    plt.legend(handles=patches, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+    img = np.empty((grid.shape[0], grid.shape[1], 3))
+    legend_elements=[]
+    for i in values:
+        # get the official Corine Land Cover RGB values and level 3 class name
+        rgb,name=get_meta(i)
+        img[np.where(grid==i)]=rgb
+        legend_elements.append(
+            Patch(facecolor=rgb, edgecolor=None,
+                         label=name)
+        )
+    plt.imshow(img, interpolation='none')
+    plt.legend(handles=legend_elements, bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0. )
+    plt.tight_layout()
+    if fp != None:
+        plt.savefig(fp)
     plt.show()
